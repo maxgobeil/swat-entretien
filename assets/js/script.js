b0678425f734e283582d276b325310ff4ab6104c
Max Gobeil <gobmaxime@gmail.com>
2025-01-08 22:01:49 -0500

    Feat: File structure & text translate

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   const toggle = document.getElementById("langToggle");
 
-  // Set initial state based on current page
-  toggle.checked = window.location.pathname.includes("/fr.html");
+  // Set initial state based on current path
+  toggle.checked = !window.location.pathname.includes("/en/");
 
   toggle.addEventListener("change", function () {
-    const newPath = this.checked ? "/fr.html" : "/index.html";
+    const newPath = this.checked ? "/" : "/en/";
     window.location.href = newPath;
   });
 });
